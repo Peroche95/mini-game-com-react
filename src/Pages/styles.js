77f3adc90e1c977502b9d579b08ca00e5ee2c7a9
Peroche95 <marllonperohe@gmail.com>
2024-06-12 09:58:24 -0300

    aplicando a estilizaçao

--- src/Pages/styles.js
+++ src/Pages/styles.js
@@ -1,13 +1,37 @@
 import styled from 'styled-components';
 
 export const AppContainer = styled.div`
+  background-color: #fff;  
+  display: flex;
+  flex-direction: column;
   text-align: center;
-  margin-top: 50px;
+  align-items: center;
+  justify-content: center;
+  max-width: 300px;
+
+  padding: 40px; 
+  border: 1px solid #ccc; 
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  margin: auto;
+  position: absolute; 
+  top: 50%;
+  left: 50%; 
+  transform: translate(-50%, -50%);
 `;
 
+
+
 export const Button = styled.button`
-  margin-top: 20px;
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
+padding: 10px 20px;
+font-size: 16px;
+cursor: pointer;
+border: none;
+border-radius: 5px;
+background-color: #007bff;
+color: white;
+transition: background-color 0.3s ease;
+
+&:hover {
+  background-color: #0056b3;
+}
 `;
